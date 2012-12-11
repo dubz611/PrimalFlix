6ca490cdaded7d18868dbad81ff4d9f993e4bb07
dubz611 <wfields611@gmail.com>
2012-12-11 18:07:49 -0500

    Updated index.php

---
+++ javascript.php
@@ -0,0 +1,12 @@
+<html>
+    <head><title>Link Test</title></head>
+    <body>
+        <a id="mylink" href="http://mysite.com">Click me</a><br />
+        <script>
+            //document.location.href = 'http://google.com'
+        url = document.links.mylink.href
+        document.write('The url is ' + url)
+    </script>
+    </body>
+</html>
+
